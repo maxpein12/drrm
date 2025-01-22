53a24a825db3174ae7bb7962d4fad9790be8c55f
Maxpein12 <bambarovie@gmail.com>
2025-01-23 01:20:45 +0800

    index1

--- resources/views/disasters/index.blade.php
+++ resources/views/disasters/index.blade.php
@@ -34,7 +34,7 @@
       
         <!-- Section-->
         <section class="py-5" style="background-color: #EEE4C9; ">
-            <div class="container px-4 px-lg-5 mt-5">
+            <div class="container px-4 px-lg-5 mt-5" >
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                   
                     <div class="description">
