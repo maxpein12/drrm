07524d3b05354d07165d1d5fd9d010f192c361f9
Maxpein12 <bambarovie@gmail.com>
2025-01-23 01:46:53 +0800

    index1

--- resources/views/disasters/index.blade.php
+++ resources/views/disasters/index.blade.php
@@ -33,7 +33,7 @@
         </nav>
       
         <!-- Section-->
-        <section class="py-5" style="background-color: #EEE4C9; ">
+        <section class="" style="background-color: #EEE4C9; padding: 50px; height: 79vh">
             <div class="container px-4 px-lg-5 mt-5 py-1" >
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                   
